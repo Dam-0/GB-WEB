126a60e122cccaeed9ab7681b19a13f3f6c28c81
Dam-0 <damien.peters2000@gmail.com>
2024-12-14 14:01:16 +0800

    Dummy DNS server 1.0

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # GB-WEB
 A web portal for GB mobile adapter connections via serial (and wifi).
 
-DNS Server (dummy atm) based of [REON](https://github.com/REONTeam/dummy-servers) Dummy Servers
+DNS Server based of [REON](https://github.com/REONTeam/dummy-servers) Dummy Servers
 
 // Current Progress
-* Dummy DNS feature (WIP) #Not Functional
+* Dummy DNS (WIP)
+
+
+// Todo
+* Dummy Pop
+* HTTP serve (very basic features)
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/binary"
 	"flag"
-
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 // For Coloured Text
@@ -17,49 +23,156 @@ var Cyan = "\033[36m"
 var Gray = "\033[37m" 
 var White = "\033[97m"
 
+func dns(port int) {
+	if len(os.Args) > 1 {
+		fmt.Sscanf(os.Args[1], "%d", &port)
+	}
 
+	server, err := net.ListenUDP("udp", &net.UDPAddr{Port: port})
+	if err != nil {
+		fmt.Printf("Port:%s%d%s is already use\n",Red,port,Reset)
+		os.Exit(1)
+	}
+	defer server.Close()
 
+	// Avoid locking up on windows
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT)
+	go func() {
+		<-signalChan
+		os.Exit(0)
+	}()
 
-// clears terminal
-func clear_term() {
-	fmt.Print("\033[H\033[2J")
+	server.SetDeadline(time.Now().Add(100 * time.Millisecond))
+
+	for {
+		message := make([]byte, 512)
+		_, address, err := server.ReadFromUDP(message)
+		if err != nil {
+			if network_error, ok := err.(net.Error); ok && network_error.Timeout() {
+				continue
+			}
+			panic(err)
+		}
+
+		var id, flags, qdcount, ancount, nscount, arcount uint16
+		binary.Read(bytes.NewReader(message[:12]), binary.BigEndian, &id)
+		binary.Read(bytes.NewReader(message[2:]), binary.BigEndian, &flags)
+		binary.Read(bytes.NewReader(message[4:]), binary.BigEndian, &qdcount)
+		binary.Read(bytes.NewReader(message[6:]), binary.BigEndian, &ancount)
+		binary.Read(bytes.NewReader(message[8:]), binary.BigEndian, &nscount)
+		binary.Read(bytes.NewReader(message[10:]), binary.BigEndian, &arcount)
+
+		// Make sure we're receiving a normal query
+		if (flags&0xFE8F) != 0 || qdcount == 0 {
+			continue
+		}
+
+		// Make result flags
+		response_flags := uint16(0x8000)
+		// If recursion is desired, set it as available
+		if flags&0x0100 != 0 {
+			response_flags |= 0x0180
+		}
+
+		// Read first query section
+		query_name, offset := read_name(message, 12)
+		var query_type, query_class uint16
+		binary.Read(bytes.NewReader(message[offset:]), binary.BigEndian, &query_type)
+		binary.Read(bytes.NewReader(message[offset+2:]), binary.BigEndian, &query_class)
+		if query_class != 1 || (query_type != 1 && query_type != 28) {
+			continue
+		}
+		response_name := make_name(query_name)
+		response_data := query(query_name, query_type)
+
+		// Encode result
+		response := new(bytes.Buffer)
+		binary.Write(response, binary.BigEndian, id)
+		binary.Write(response, binary.BigEndian, response_flags)
+		binary.Write(response, binary.BigEndian, uint16(1))
+		binary.Write(response, binary.BigEndian, uint16(1))
+		binary.Write(response, binary.BigEndian, uint16(0))
+		binary.Write(response, binary.BigEndian, uint16(0))
+		response.Write(response_name)
+		binary.Write(response, binary.BigEndian, query_type)
+		binary.Write(response, binary.BigEndian, query_class)
+		response.Write([]byte{0xc0, 0x0c})
+		binary.Write(response, binary.BigEndian, query_type)
+		binary.Write(response, binary.BigEndian, query_class)
+		binary.Write(response, binary.BigEndian, uint32(0))
+		binary.Write(response, binary.BigEndian, uint16(len(response_data)))
+		response.Write(response_data)
+		server.WriteToUDP(response.Bytes(), address)
+	}
 }
 
-// Query
-func Query(name string, qtype int) (ip []byte) {
-
-	if qtype == 1 { // A (IPv4)
-		if name == "example.com" {
-			ip =[]byte{93, 184, 216, 34}
+func query(query_name string, query_type uint16) []byte {
+	var ip []byte
+	if query_type == 1 { // A (ipv4)
+		if query_name == "example.com" {
+			ip = []byte{93, 184, 216, 34}
 		} else {
 			ip = []byte{127, 0, 0, 1}
 		}
-	} else if qtype == 28 { // AAAA (IPv6)
-		if name == "example.com" {
-			ip =[]byte{0x26, 0x06, 0x28, 0x00, 0x02, 0x20, 0x00, 0x01,0x02, 0x48, 0x18, 0x93, 0x25, 0xc8, 0x19, 0x46}
+	} else if query_type == 28 { // AAAA (ipv6)
+		if query_name == "example.com" {
+			ip = []byte{0x26, 0x06, 0x28, 0x00, 0x02, 0x20, 0x00, 0x01,
+				0x02, 0x48, 0x18, 0x93, 0x25, 0xc8, 0x19, 0x46}
 		} else {
-			ip = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01}
+			ip = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01}
 		}
 	}
-	//fmt.Printf("%s = %d", name,ip)
-	return 
+	fmt.Println(query_name, "=", ip)
+	return ip
 }
 
+func make_name(query_name string) []byte {
+	var response bytes.Buffer
+	for _, x := range bytes.Split([]byte(query_name), []byte{'.'}) {
+		response.WriteByte(byte(len(x)))
+		response.Write(x)
+	}
+	response.WriteByte(0)
+	return response.Bytes()
+}
+
+func read_name(data []byte, offset int) (string, int) {
+	var response string
+	for {
+		length := data[offset]
+		offset++
+		if length == 0 {
+			break
+		}
+		if response != "" {
+			response += "."
+		}
+		response += string(data[offset : offset+int(length)])
+		offset += int(length)
+	}
+	return response, offset
+}
 
 func main() {
+	// Channel for goroutine syncs
+	finish := make(chan bool)
 
 	// Set Arguements 
-	DNS_port := flag.Int("DNS Port", 53, "an int")
-
+	dns_port := flag.Int("dport", 53, "Current Port used for DNS server")
     flag.Parse()
+	
+	// Run Everything
+	go func() {
+		dns(*dns_port)
+	} ()
 
-	fmt.Printf("Server: 127.0.0.1:%d \n",*DNS_port)
+	// Very Dirty Hack
+	time.Sleep(100 * time.Millisecond)
 
-	fmt.Println()
+	// Provide Information
+	fmt.Printf("DNS Server: 127.0.0.1:%s%d%s \n",Green,*dns_port,Reset)
 
-	//test value
-	ip := Query("example.com", 1)
-
-	fmt.Printf("IP: %d", ip)
-
+	<-finish // Routines to finish
 }
