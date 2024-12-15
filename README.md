308d11b08df102899eb452ad23a34f39558066b8
Dam-0 <damien.peters2000@gmail.com>
2024-12-15 12:41:51 +0800

    Actually work DNS and POP server

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 # GB-WEB
-A web portal for GB mobile adapter connections via serial (and wifi).
+A web portal for GB mobile adapter connections via serial (and wifi). (not yet)
 
-DNS Server based of [REON](https://github.com/REONTeam/dummy-servers) Dummy Servers
+Dummy Servers based of [REON](https://github.com/REONTeam/dummy-servers) work. (pretty much 1 for 1 atm)
+
+
+Both DNS server and POP run at the same time
 
 // Current Progress
 * Dummy DNS (WIP)
+* Dummy Pop (WIP)
 
 
 // Todo
-* Dummy Pop
-* HTTP serve (very basic features)
+* HTTP server (very basic features)
+
+
+// How to use
+
+Will run on Port 53 and 110 by default
+```go
+go run .\main.go
+
+// To specify custom DNS port
+go run .\main.go -dport 55
+```
--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 )
 
@@ -23,31 +21,27 @@ var Cyan = "\033[36m"
 var Gray = "\033[37m" 
 var White = "\033[97m"
 
-func dns(port int) {
-	if len(os.Args) > 1 {
-		fmt.Sscanf(os.Args[1], "%d", &port)
-	}
+func dns(port string) {
 
-	server, err := net.ListenUDP("udp", &net.UDPAddr{Port: port})
+	port = ":" + port
+
+	server, err := net.ResolveUDPAddr("udp4", port)
 	if err != nil {
-		fmt.Printf("Port:%s%d%s is already use\n",Red,port,Reset)
+			fmt.Println(err)
+			return
+	}
+	
+	dns_server, err := net.ListenUDP("udp4", server)
+
+	if err != nil {
+		fmt.Printf("Port:%s%s%s is already use\n",Red,port,Reset)
 		os.Exit(1)
 	}
-	defer server.Close()
-
-	// Avoid locking up on windows
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT)
-	go func() {
-		<-signalChan
-		os.Exit(0)
-	}()
-
-	server.SetDeadline(time.Now().Add(100 * time.Millisecond))
+	defer dns_server.Close()
 
 	for {
-		message := make([]byte, 512)
-		_, address, err := server.ReadFromUDP(message)
+		buffer := make([]byte, 512)
+		_, address, err := dns_server.ReadFromUDP(buffer)
 		if err != nil {
 			if network_error, ok := err.(net.Error); ok && network_error.Timeout() {
 				continue
@@ -56,12 +50,12 @@ func dns(port int) {
 		}
 
 		var id, flags, qdcount, ancount, nscount, arcount uint16
-		binary.Read(bytes.NewReader(message[:12]), binary.BigEndian, &id)
-		binary.Read(bytes.NewReader(message[2:]), binary.BigEndian, &flags)
-		binary.Read(bytes.NewReader(message[4:]), binary.BigEndian, &qdcount)
-		binary.Read(bytes.NewReader(message[6:]), binary.BigEndian, &ancount)
-		binary.Read(bytes.NewReader(message[8:]), binary.BigEndian, &nscount)
-		binary.Read(bytes.NewReader(message[10:]), binary.BigEndian, &arcount)
+		binary.Read(bytes.NewReader(buffer[:12]), binary.BigEndian, &id)
+		binary.Read(bytes.NewReader(buffer[2:]), binary.BigEndian, &flags)
+		binary.Read(bytes.NewReader(buffer[4:]), binary.BigEndian, &qdcount)
+		binary.Read(bytes.NewReader(buffer[6:]), binary.BigEndian, &ancount)
+		binary.Read(bytes.NewReader(buffer[8:]), binary.BigEndian, &nscount)
+		binary.Read(bytes.NewReader(buffer[10:]), binary.BigEndian, &arcount)
 
 		// Make sure we're receiving a normal query
 		if (flags&0xFE8F) != 0 || qdcount == 0 {
@@ -76,10 +70,11 @@ func dns(port int) {
 		}
 
 		// Read first query section
-		query_name, offset := read_name(message, 12)
+		query_name, offset := read_name(buffer, 12)
 		var query_type, query_class uint16
-		binary.Read(bytes.NewReader(message[offset:]), binary.BigEndian, &query_type)
-		binary.Read(bytes.NewReader(message[offset+2:]), binary.BigEndian, &query_class)
+		binary.Read(bytes.NewReader(buffer[offset:]), binary.BigEndian, &query_type)
+		binary.Read(bytes.NewReader(buffer[offset+2:]), binary.BigEndian, &query_class)
+
 		if query_class != 1 || (query_type != 1 && query_type != 28) {
 			continue
 		}
@@ -103,7 +98,7 @@ func dns(port int) {
 		binary.Write(response, binary.BigEndian, uint32(0))
 		binary.Write(response, binary.BigEndian, uint16(len(response_data)))
 		response.Write(response_data)
-		server.WriteToUDP(response.Bytes(), address)
+		dns_server.WriteToUDP(response.Bytes(), address)
 	}
 }
 
@@ -117,11 +112,9 @@ func query(query_name string, query_type uint16) []byte {
 		}
 	} else if query_type == 28 { // AAAA (ipv6)
 		if query_name == "example.com" {
-			ip = []byte{0x26, 0x06, 0x28, 0x00, 0x02, 0x20, 0x00, 0x01,
-				0x02, 0x48, 0x18, 0x93, 0x25, 0xc8, 0x19, 0x46}
+			ip = []byte{0x26, 0x06, 0x28, 0x00, 0x02, 0x20, 0x00, 0x01, 0x02, 0x48, 0x18, 0x93, 0x25, 0xc8, 0x19, 0x46}
 		} else {
-			ip = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01}
+			ip = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01}
 		}
 	}
 	fmt.Println(query_name, "=", ip)
@@ -155,24 +148,76 @@ func read_name(data []byte, offset int) (string, int) {
 	return response, offset
 }
 
+
+func pop() {
+
+	pop_listener, err := net.Listen("tcp", ":110")
+	if err != nil {
+		fmt.Printf("Port:%s110%s is already use\n",Red,Reset)
+		os.Exit(1)
+	}
+	defer pop_listener.Close()
+
+
+	pop_listener.(*net.TCPListener).SetDeadline(time.Now().Add(time.Second))
+
+	for {
+		connection, err := pop_listener.Accept()
+		if err != nil {
+			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				continue
+			}
+			panic(err)
+		}
+
+		connection.Write([]byte("+OK\r\n"))
+
+		buffer := make([]byte, 1024)
+		n, err := connection.Read(buffer)
+		if err != nil {
+			break
+		}
+		msg := string(buffer[:n])
+		fmt.Print(msg)
+
+		switch {
+		case msg[:4] == "USER":
+			connection.Write([]byte("+OK\r\n"))
+		case msg[:4] == "PASS":
+			connection.Write([]byte("+OK\r\n"))
+		case msg[:5] == "STAT":
+			connection.Write([]byte("+OK 0 0\r\n"))
+		case msg[:4] == "QUIT":
+			connection.Write([]byte("+OK\r\n"))
+		}
+	}
+}
+
 func main() {
 	// Channel for goroutine syncs
 	finish := make(chan bool)
 
 	// Set Arguements 
-	dns_port := flag.Int("dport", 53, "Current Port used for DNS server")
+	dns_port := flag.String("dport", "53", "Current Port used for DNS server")
     flag.Parse()
 	
 	// Run Everything
+	
+	// DNS Server
 	go func() {
 		dns(*dns_port)
+	} ()
+
+	// POP Server
+	go func() {
+		pop()
 	} ()
 
 	// Very Dirty Hack
 	time.Sleep(100 * time.Millisecond)
 
 	// Provide Information
-	fmt.Printf("DNS Server: 127.0.0.1:%s%d%s \n",Green,*dns_port,Reset)
+	fmt.Printf("DNS Server: 127.0.0.1:%s%s%s \n",Green,*dns_port,Reset)
 
 	<-finish // Routines to finish
 }
